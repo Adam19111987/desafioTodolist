9e44cc13d5784eb1b0c5d576bbc86779f6041bd4
AdamDev <a.diaz.1987.08@hotmail.com>
2024-09-25 12:58:26 -0300

    trabajo finalizado

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,5 +1,3 @@
-
-
 const inputTarea = document.querySelector("#inputCards");
 const btnTarea = document.querySelector("#btnagregar");
 const totalTareas = document.querySelector("#total");
@@ -10,32 +8,64 @@ const Tareas = [];
 btnTarea.addEventListener("click", () => {
   const nuevaTarea = inputTarea.value;
   const ident = Math.floor(Math.random() * 90) + 10;
-  Tareas.push({ id: ident, tarea: nuevaTarea });
+  Tareas.push({ id: ident, tarea: nuevaTarea, realizadas: false });
   inputTarea.value = "";
   renderizarTareas(Tareas, "#listID", "#listTarea", "#inputcheck");
-  totalTareas.innerHTML = `${Tareas.length}`;
+  
+
 });
 
-function renderizarTareas(labores, elemenID, elementarea,elementchech) {
-  let elemento = document.querySelector(`${elemenID}`);
-  let  elemenTarea = document.querySelector(`${elementarea}`);
-  let elementchek = document.querySelector(`${elementchech}`)
-  
- 
+function renderizarTareas(labores, elemenID, elementarea, elementchech) {
+  let elementoID = document.querySelector(`${elemenID}`);
+  let elementoTarea = document.querySelector(`${elementarea}`);
+  let elementocheck = document.querySelector(`${elementchech}`);
+
   let htmlID = "";
-  let htmltarea = "";
-  let htmlchek = "";
-  for (labor of labores) {
-    htmlID += `<li class ="listaid">${labor.id}</li> `;
-    htmltarea += `<li class ="listaid">${labor.tarea}</li>`;
-    htmlchek += ` <input type="checkbox">`
+  let htmlTarea = "";
+  let htmlCheck = "";
+    
+
+ 
+  labores.forEach((labor, index) =>{
+  htmlID += `<li class="listaid">${labor.id}</li>`;
+  htmlTarea += `<li class="listatarea">${labor.tarea}</li>`;
+  htmlCheck += `<div class = "bntcheks"
+  ><input class="activarCheck"  type="checkbox" data-index="${index}" ${labor.completada ? 'checked' : ''}><i onclick="borrar(${labor.id})" class="fa-solid fa-x btnX"></i></div>`;
+ 
+ });
+
+ totalTareas.innerHTML = Tareas.length === 0 ? '0' : `${Tareas.length}`;
+
+ elementoID.innerHTML = htmlID;
+ elementoTarea.innerHTML = htmlTarea;
+ elementocheck.innerHTML = htmlCheck;
+
+ let checkboxes = document.querySelectorAll('.activarCheck');
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("change", (event) => {
+      const index = event.target.getAttribute("data-index");
+      Tareas[index].completada = event.target.checked;
+      actualizarTareasRealizadas();
+     })
+  });
+ 
+ }
+
+ function borrar(id) {
+  const index = Tareas.findIndex((ele) => ele.id === id);
+  if (index !== -1) {
+    Tareas.splice(index, 1);
+    renderizarTareas(Tareas, "#listID", "#listTarea", "#inputcheck");
+
+  
+    
   }
-  elemento.innerHTML = htmlID;
-  elemenTarea.innerHTML = htmltarea;
-  elementchek.innerHTML = htmlchek;
 }
+  
 
 
-function borrarid (Tareas){
-   
+
+function actualizarTareasRealizadas() {
+  const taksreadys = Tareas.filter((tarea) => tarea.completada).length;
+  tareasRealizdas.innerHTML = `${taksreadys}`;
 }
